706e323cec545507f2df7f1dd4076a6174f4f009
Daniel Lucena Ovalle <lucenadaniel20@gmail.com>
2025-01-27 13:15:55 -0500

    fetch second part

--- fetch.js
+++ fetch.js
@@ -1,8 +1,21 @@
-const link  = "https://jsonplaceholder.typicode.com/posts/1";
+const link  = "https://jsonplaceholder.typicode.com/posts/";
 
 
-fetch(link)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((e) => console.log(e))
-    .finally(() => console.log("finalizo"));
+// fetch(link)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((e) => console.log(e))
+//     .finally(() => console.log("finalizo"));
+
+const findPostById = async (id) => {
+    try {
+        const res = await fetch(link + id)
+        const post = await res.json()
+        console.log(post)
+    } catch (error){
+        console.log(error)
+    }
+    
+}
+
+findPostById (2)
